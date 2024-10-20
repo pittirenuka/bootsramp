28cb58cace2403f3a007e1268ddc718e0d1082b5
renuka pitti <renukapitti515@gmail.com>
2024-10-20 16:30:55 +0530

    arsha

---
+++ sample.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.client').slick({
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,      
+    });
+  });
